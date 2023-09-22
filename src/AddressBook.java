1088d37f9aaa0ac4193eaaf764ada626f2ce1ab4
melvi <melvinqu@cmail.carleton.ca>
2023-09-22 15:26:07 -0400

    create buddyinfo add then remove from addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -7,7 +7,10 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("tom","carleton","613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
     public void addBuddy(BuddyInfo buddy){
